ac302d69a403fe54c0a801f0959873de88717c65
minhbro3003 <minhtiencool03@gmail.com>
2024-10-03 17:28:38 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import "antd/dist/antd.min.css";
+import "antd/dist/reset.css";
+// import store from "./app/store";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+);
+
+reportWebVitals();
